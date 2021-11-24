c547af5984336ffb657151564baacd74dda2266b
Maciej Lipski <maciej.lipski@capgemini.com>
2021-11-24 20:20:54 +0100

    Publishing events with InventoryLoggingEventPublisher

--- src/main/java/com/capgemini/sample/sf/inventory/InventoryFacade.java
+++ src/main/java/com/capgemini/sample/sf/inventory/InventoryFacade.java
@@ -3,12 +3,15 @@ package com.capgemini.sample.sf.inventory;
 import com.capgemini.sample.sf.infrastructure.Registrator;
 import com.capgemini.sample.sf.inventory.dto.ItemChangeDto;
 import com.capgemini.sample.sf.inventory.dto.ItemDto;
+import com.capgemini.sample.sf.inventory.event.BelowThresholdEvent;
+import com.capgemini.sample.sf.inventory.event.NoItemOnStockEvent;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class InventoryFacade {
 
+    private static final int STOCK_THRESHOLD = 10;
     private final InventoryRepository repository;
     private final InventoryEventPublisher publisher;
 
@@ -41,5 +44,11 @@ public class InventoryFacade {
                 .orElseThrow(() -> new ItemNotFoundException(id));
         item.update(itemChangeDto);
         repository.save(item);
+
+        if (item.getQuantity() == 0) {
+            publisher.publish(new NoItemOnStockEvent(item.asDto()));
+        } else if (item.getQuantity() <= STOCK_THRESHOLD) {
+            publisher.publish(new BelowThresholdEvent(item.asDto(), STOCK_THRESHOLD));
+        }
     }
 }
--- src/main/java/com/capgemini/sample/sf/inventory/event/BelowThresholdEvent.java
+++ src/main/java/com/capgemini/sample/sf/inventory/event/BelowThresholdEvent.java
@@ -2,18 +2,21 @@ package com.capgemini.sample.sf.inventory.event;
 
 import com.capgemini.sample.sf.inventory.dto.ItemDto;
 
-public class NoItemOnStockEvent extends InventoryEvent {
+public class BelowThresholdEvent extends InventoryEvent {
 
     private final ItemDto itemDto;
+    private final int threshold;
 
-    public NoItemOnStockEvent(ItemDto itemDto) {
+    public BelowThresholdEvent(ItemDto itemDto, int threshold) {
         this.itemDto = itemDto;
+        this.threshold = threshold;
     }
 
     @Override
     public String toString() {
-        return "NoItemOnStockEvent{" +
+        return "BelowThresholdEvent{" +
                 "itemDto=" + itemDto +
+                ", threshold=" + threshold +
                 "} " + super.toString();
     }
 }
