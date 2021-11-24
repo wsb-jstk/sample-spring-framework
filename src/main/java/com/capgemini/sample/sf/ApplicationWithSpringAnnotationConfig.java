93e82bd7d1803fdf1913fee2c7bd5104629ddcc6
Maciej Lipski <maciej.lipski@capgemini.com>
2021-11-24 20:43:40 +0100

    Added ApplicationWithSpringAnnotationConfig

---
+++ src/main/java/com/capgemini/sample/sf/ApplicationWithSpringAnnotationConfig.java
@@ -0,0 +1,39 @@
+package com.capgemini.sample.sf;
+
+import com.capgemini.sample.sf.inventory.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class ApplicationWithSpringAnnotationConfig {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfiguration.class);
+
+        InventorySampleData.initSampleItems(context.getBean(InventoryRepository.class));
+
+        new ApplicationRunner().run(context.getBean(InventoryFacade.class));
+    }
+
+
+    @Configuration(proxyBeanMethods = true)
+    public static class SpringConfiguration {
+
+        @Bean(name = {"myName", "drugaNazwa", "inventoryRepository"})
+        InventoryRepository inventoryRepository() {
+            return new InMemoryInventoryRepository();
+        }
+
+        @Bean
+        InventoryEventPublisher inventoryEventPublisher() {
+            return new InventoryLoggingEventPublisher();
+        }
+
+        @Bean
+        InventoryFacade inventoryFacade() {
+            return new InventoryFacade(inventoryRepository(), inventoryEventPublisher());
+        }
+
+    }
+
+}
