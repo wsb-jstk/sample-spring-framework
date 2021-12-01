9ad8f61c774024fce95d03c3663808a229ee4f40
Maciej Lipski <maciej.lipski@capgemini.com>
2021-12-01 20:10:00 +0100

    Example of SpringBootServletInitializer and @ServletComponentScan (to read the @WebServlet

--- src/main/java/com/capgemini/sample/sf/ApplicationWithSpringBoot.java
+++ src/main/java/com/capgemini/sample/sf/ApplicationWithSpringBoot.java
@@ -2,12 +2,21 @@ package com.capgemini.sample.sf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationWithSpringBoot {
+@ServletComponentScan
+public class ApplicationWithSpringBoot extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         new SpringApplication().run(ApplicationWithSpringBoot.class);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+        return application.sources(ApplicationWithSpringBoot.class);
     }
 
 }
